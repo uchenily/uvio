36e4c3084d8f5d768b9db98ce16c5db5f6696760
uchenily <uchenily@qq.com>
2024-05-06 17:54:18 +0800

    gtest TEST_P

--- tests/gtests/gtest_foo.cpp
+++ tests/gtests/gtest_foo.cpp
@@ -29,3 +29,28 @@ TEST_F(TestFixtureName, TestName) {
 TEST_F(TestFixtureName, TestName2) {
     EXPECT_EQ(vec.size(), 3);
 }
+
+// ===== TEST_P ===== (P: parameterized)
+
+class TestSuiteParameterized : public testing::TestWithParam<int> {};
+
+auto IsPrime(int num) -> bool {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+INSTANTIATE_TEST_CASE_P(TestSuiteParameterizedName,
+                        TestSuiteParameterized,
+                        testing::Values(3, 5, 11, 23, 17));
+
+TEST_P(TestSuiteParameterized, TestName) {
+    int n = GetParam();
+    EXPECT_TRUE(IsPrime(n));
+}
