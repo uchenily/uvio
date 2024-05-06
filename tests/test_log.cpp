cb6a484de3b7f37f9262601bf1c8561b786fd40d
uchenily <uchenily@qq.com>
2024-05-06 14:19:20 +0800

    log methods

--- tests/test_log.cpp
+++ tests/test_log.cpp
@@ -1,6 +1,9 @@
 #include "uvio/log.hpp"
 
 auto main() -> int {
+    console.trace("hello world");
+    console.debug("hello world");
     console.info("hello world");
-    console.info("hello {}", "zhangsan");
+    console.warn("hello world");
+    console.error("hello world");
 }
--- uvio/log.hpp
+++ uvio/log.hpp
@@ -5,9 +5,33 @@
 class ConsoleLogger {
 public:
     template <typename... Args>
+    void trace(std::string_view fmt, Args... args) {
+        auto msg = std::vformat(fmt, std::make_format_args(args...));
+        std::cout << "[TRACE] " << msg << '\n';
+    }
+
+    template <typename... Args>
+    void debug(std::string_view fmt, Args... args) {
+        auto msg = std::vformat(fmt, std::make_format_args(args...));
+        std::cout << "[DEBUG] " << msg << '\n';
+    }
+
+    template <typename... Args>
     void info(std::string_view fmt, Args... args) {
         auto msg = std::vformat(fmt, std::make_format_args(args...));
-        std::cout << msg << '\n';
+        std::cout << "[INFO ] " << msg << '\n';
+    }
+
+    template <typename... Args>
+    void warn(std::string_view fmt, Args... args) {
+        auto msg = std::vformat(fmt, std::make_format_args(args...));
+        std::cout << "[WARN ] " << msg << '\n';
+    }
+
+    template <typename... Args>
+    void error(std::string_view fmt, Args... args) {
+        auto msg = std::vformat(fmt, std::make_format_args(args...));
+        std::cout << "[ERROR] " << msg << '\n';
     }
 };
 
