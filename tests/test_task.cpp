92ee708233f6079578c4e29cb6d8ec0443b113b7
uchenily <uchenily@qq.com>
2024-05-06 20:44:07 +0800

    test Task

--- tests/test_task.cpp
+++ tests/test_task.cpp
@@ -9,10 +9,31 @@ auto test() -> Task<> {
     co_return;
 }
 
+auto test_call3() -> Task<> {
+    console.info("test_call3");
+    co_return;
+}
+
+auto test_call2() -> Task<> {
+    console.info("test_call2 start");
+    co_await test_call3();
+    console.info("test_call2 end");
+}
+
+auto test_call() -> Task<> {
+    console.info("test_call1 start");
+    co_await test_call2();
+    console.info("test_call1 end");
+}
+
 auto main() -> int {
     auto task = test();
     auto handle = task.take();
     console.info("before resume");
     handle.resume();
     console.info("after resume");
+
+    auto task2 = test_call();
+    auto handle2 = task2.take();
+    handle2.resume();
 }
