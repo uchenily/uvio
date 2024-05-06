3d4f72663c8b5e14377bd92a81348552c4713fed
uchenily <uchenily@qq.com>
2024-05-06 17:42:25 +0800

    gtest TEST_F

--- tests/gtests/gtest_foo.cpp
+++ tests/gtests/gtest_foo.cpp
@@ -1,7 +1,31 @@
 #include "gtest/gtest.h"
 
+// ===== TEST =====
+
 TEST(TestSuiteName, TestName) {
     EXPECT_EQ(2, [](int a, int b) {
         return a + b;
     }(1, 1));
+}
+
+// ===== TEST_F =====
+
+class TestFixtureName : public testing::Test {
+protected:
+    void SetUp() override {
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3);
+    }
+    std::vector<int> vec;
+};
+
+TEST_F(TestFixtureName, TestName) {
+    vec.push_back(4);
+    EXPECT_EQ(vec.size(), 4);
+    EXPECT_EQ(vec.back(), 4);
+}
+
+TEST_F(TestFixtureName, TestName2) {
+    EXPECT_EQ(vec.size(), 3);
 }
