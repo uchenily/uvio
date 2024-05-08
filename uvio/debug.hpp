f13f6cda227eae113e56ab6a6816549a21883fcf
uchenily <uchenily@qq.com>
2024-05-08 23:25:22 +0800

    debug: log error at any time

--- uvio/debug.hpp
+++ uvio/debug.hpp
@@ -4,9 +4,9 @@
 
 #include "uv.h"
 
-#ifndef NDEBUG
-
 static inline auto &debug_logger = uvio::log::console;
+
+#ifndef NDEBUG
 
 #define SET_LOG_LEVEL(level) debug_logger.set_level(level)
 #define SET_LOG_STYLE(style) debug_logger.set_style(style)
@@ -14,8 +14,6 @@ static inline auto &debug_logger = uvio::log::console;
 #define LOG_INFO(...) debug_logger.info(__VA_ARGS__)
 #define LOG_DEBUG(...) debug_logger.debug(__VA_ARGS__)
 #define LOG_WARN(...) debug_logger.warn(__VA_ARGS__)
-#define LOG_ERROR(...) debug_logger.error(__VA_ARGS__)
-#define LOG_FATAL(...) debug_logger.fatal(__VA_ARGS__)
 
 #else
 
@@ -25,10 +23,11 @@ static inline auto &debug_logger = uvio::log::console;
 #define LOG_INFO(...)
 #define LOG_DEBUG(...)
 #define LOG_WARN(...)
-#define LOG_ERROR(...)
-#define LOG_FATAL(...)
 
 #endif
+
+#define LOG_ERROR(...) debug_logger.error(__VA_ARGS__)
+#define LOG_FATAL(...) debug_logger.fatal(__VA_ARGS__)
 
 static inline auto uv_check(int retval) {
     if (retval != 0) {
