0dc8c8e3f48304573eb6bbbe58161bfb36b2bd10
uchenily <uchenily@qq.com>
2024-05-09 20:23:12 +0800

    fix #2

---
+++ tests/test_coredump.cpp
@@ -0,0 +1,49 @@
+#include "uvio/core.hpp"
+#include "uvio/net.hpp"
+#include "uvio/sync.hpp"
+
+using namespace uvio;
+using namespace uvio::sync;
+using namespace uvio::net;
+
+auto main() -> int {
+    auto server = [](Latch &start, Latch &finish) -> Task<> {
+        std::array<char, 64> buf{};
+
+        auto listener = TcpListener();
+        listener.bind("localhost", 12345);
+        start.count_down();
+
+        auto stream = co_await listener.accept();
+        auto nread = co_await stream.read(buf);
+        // EXPECT_EQ(nread, 12);
+        // EXPECT_STREQ(buf.data(), "test message");
+        co_await stream.write(buf.data());
+
+        // finish.count_down();
+    };
+
+    auto client = [](Latch &start, Latch &finish) -> Task<> {
+        std::array<char, 64> buf{};
+
+        co_await start.arrive_and_wait();
+        auto stream = co_await TcpStream::connect("localhost", 12345);
+        co_await stream.write("test message");
+        auto nread = co_await stream.read(buf);
+        // EXPECT_EQ(nread, 12);
+        // EXPECT_STREQ(buf.data(), "test message");
+
+        // finish.count_down();
+    };
+
+    auto test = [&]() -> Task<> {
+        Latch start(3);
+        Latch finish(3);
+        spawn(server(start, finish));
+        spawn(client(start, finish));
+        co_await start.arrive_and_wait();
+        // co_await finish.arrive_and_wait();
+    };
+
+    block_on(test());
+}
