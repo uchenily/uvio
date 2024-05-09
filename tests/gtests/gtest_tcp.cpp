62c7c6dbb02033fa13dbff16a02ee2d469ffb9af
uchenily <uchenily@qq.com>
2024-05-09 17:20:24 +0800

    fix ci

--- tests/gtests/gtest_tcp.cpp
+++ tests/gtests/gtest_tcp.cpp
@@ -9,38 +9,44 @@ using namespace uvio::net;
 using namespace uvio::sync;
 
 TEST(TestTcpListener, ListenAndAccept) {
-    Latch latch(3);
+    Latch start(3);
+    Latch finish(3);
 
-    auto server = [&latch]() -> Task<> {
+    auto server = [&]() -> Task<> {
         std::array<char, 64> buf{};
 
         auto listener = TcpListener();
         listener.bind("localhost", 12345);
-        latch.count_down();
+        start.count_down();
 
         auto stream = co_await listener.accept();
         auto nread = co_await stream.read(buf);
         EXPECT_EQ(nread, 12);
         EXPECT_STREQ(buf.data(), "test message");
         co_await stream.write(buf.data());
+
+        finish.count_down();
     };
 
-    auto client = [&latch]() -> Task<> {
+    auto client = [&]() -> Task<> {
         std::array<char, 64> buf{};
 
-        co_await latch.arrive_and_wait();
+        co_await start.arrive_and_wait();
         auto stream = co_await TcpStream::connect("localhost", 12345);
         co_await stream.write("test message");
         auto nread = co_await stream.read(buf);
         EXPECT_EQ(nread, 12);
         EXPECT_STREQ(buf.data(), "test message");
+
+        finish.count_down();
     };
 
-    auto wait = [](Latch &latch) -> Task<> {
-        co_await latch.arrive_and_wait();
+    auto wait = [](Latch &start, Latch &finish) -> Task<> {
+        co_await start.arrive_and_wait();
+        co_await finish.arrive_and_wait();
     };
 
     spawn(server());
     spawn(client());
-    block_on(wait(latch));
+    block_on(wait(start, finish));
 }
---
+++ uvio/sync.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "uvio/sync/latch.hpp"
