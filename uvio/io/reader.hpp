d48097b4af365d8c97257716bd0ed99e6f362e70
uchenily <uchenily@qq.com>
2024-05-21 15:38:03 +0800

    BufReader

---
+++ uvio/io/reader.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "uvio/io/buffer.hpp"
+#include "uvio/io/impl/buf_reader.hpp"
+#include <memory>
+
+namespace uvio::io {
+
+template <typename IO, int RBUF_SIZE>
+    requires requires(IO io, std::span<char> buf) {
+        { io.read(buf) };
+    }
+class BufReader : public detail::ImplBufRead<IO> {
+public:
+    BufReader(IO &&io)
+        : io_{std::move(io)} {}
+
+    BufReader(BufReader &&other) noexcept
+        : io_{std::move(other.io_)}
+        , r_stream_{std::move(other.r_stream_)} {}
+
+    auto operator=(BufReader &&other) noexcept -> BufReader & {
+        if (std::addressof(other) != this) {
+            io_ = std::move(other.io_);
+            r_stream_ = std::move(other.r_stream_);
+        }
+        return *this;
+    }
+
+public:
+    auto inner() noexcept -> IO & {
+        return io_;
+    }
+
+    auto into_inner() noexcept -> IO {
+        r_stream_.disable();
+        return std::move(io_);
+    }
+
+private:
+    IO                              io_;
+    detail::StreamBuffer<RBUF_SIZE> r_stream_;
+};
+} // namespace uvio::io
